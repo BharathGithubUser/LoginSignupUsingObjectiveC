d6e52cfbed2a4872ea7907fd4e0b2162522cfab9
BharathGithubUser <sbharathrao1995@gmail.com>
2018-03-09 18:06:51 +0530

    Initial Commit

---
+++ LoginScreenTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoginScreenTask
+//
+//  Created by user on 09/03/18.
+//  Copyright © 2018 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LoginScreenTask/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LoginScreenTask
+//
+//  Created by user on 09/03/18.
+//  Copyright © 2018 user. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
